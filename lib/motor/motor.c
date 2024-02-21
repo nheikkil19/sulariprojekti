fb313b7db591cbcaab6b00ad0bbfd7c2e18617b0
Niko Heikkilä <niko.heikkila13@gmail.com>
2024-02-21 16:21:27 +0200

    motor correct directions

--- lib/motor/motor.c
+++ lib/motor/motor.c
@@ -36,11 +36,11 @@ void drive_backwards(uint8_t pwm) {
 
 void drive_left(uint8_t pwm) {
     // A clockwise
-    HAL_GPIO_WritePin(MD_AIN1_GPIO_Port, MD_AIN1_Pin, 1);
-    HAL_GPIO_WritePin(MD_AIN2_GPIO_Port, MD_AIN2_Pin, 0);
+    HAL_GPIO_WritePin(MD_AIN1_GPIO_Port, MD_AIN1_Pin, 0);
+    HAL_GPIO_WritePin(MD_AIN2_GPIO_Port, MD_AIN2_Pin, 1);
     // B clockwise
-    HAL_GPIO_WritePin(MD_BIN1_GPIO_Port, MD_BIN1_Pin, 1);
-    HAL_GPIO_WritePin(MD_BIN2_GPIO_Port, MD_BIN2_Pin, 0);
+    HAL_GPIO_WritePin(MD_BIN1_GPIO_Port, MD_BIN1_Pin, 0);
+    HAL_GPIO_WritePin(MD_BIN2_GPIO_Port, MD_BIN2_Pin, 1);
     // PWM duty cycle
     TIM3->CCR2 = (uint16_t)(pwm / 100.f * TIM3->ARR);
     TIM10->CCR1 = (uint16_t)(pwm / 100.f * TIM10->ARR);
@@ -51,11 +51,11 @@ void drive_left(uint8_t pwm) {
 }
 void drive_right(uint8_t pwm) {
     // A counterclockwise
-    HAL_GPIO_WritePin(MD_AIN1_GPIO_Port, MD_AIN1_Pin, 0);
-    HAL_GPIO_WritePin(MD_AIN2_GPIO_Port, MD_AIN2_Pin, 1);
+    HAL_GPIO_WritePin(MD_AIN1_GPIO_Port, MD_AIN1_Pin, 1);
+    HAL_GPIO_WritePin(MD_AIN2_GPIO_Port, MD_AIN2_Pin, 0);
     // B counterclockwise
-    HAL_GPIO_WritePin(MD_BIN1_GPIO_Port, MD_BIN1_Pin, 0);
-    HAL_GPIO_WritePin(MD_BIN2_GPIO_Port, MD_BIN2_Pin, 1);
+    HAL_GPIO_WritePin(MD_BIN1_GPIO_Port, MD_BIN1_Pin, 1);
+    HAL_GPIO_WritePin(MD_BIN2_GPIO_Port, MD_BIN2_Pin, 0);
     // PWM duty cycle
     TIM3->CCR2 = (uint16_t)(pwm / 100.f * TIM3->ARR);
     TIM10->CCR1 = (uint16_t)(pwm / 100.f * TIM10->ARR);
